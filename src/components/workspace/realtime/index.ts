96038c60f581b6b2b0fbbb2e1e700887b6169e14
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-10 13:15:53 +0500

    Add Realtime Components Exports for HuddleButton, ActivityStream, and PresenceIndicator

---
+++ src/components/workspace/realtime/index.ts
@@ -0,0 +1,4 @@
+// <== REALTIME COMPONENTS EXPORTS ==>
+export { default as HuddleButton } from "./HuddleButton";
+export { default as ActivityStream } from "./ActivityStream";
+export { default as PresenceIndicator } from "./PresenceIndicator";
