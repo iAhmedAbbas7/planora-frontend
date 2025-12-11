a09252d5121c57968a8e0af3de97db77946fa583
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-11 15:20:12 +0500

    Remove showSearch Prop from DashboardHeader in GitHubReleasesPage for UI Consistency

--- src/pages/GitHubReleasesPage.tsx
+++ src/pages/GitHubReleasesPage.tsx
@@ -1628,7 +1628,6 @@ const GitHubReleasesPage = (): JSX.Element => {
       <DashboardHeader
         title="Releases & Tags"
         subtitle={`${owner}/${repo}`}
-        showSearch={false}
       />
       {/* MAIN CONTENT */}
       <main className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
