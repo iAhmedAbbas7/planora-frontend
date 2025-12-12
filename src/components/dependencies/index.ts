779823d983bd59db7ee8c2c763a98fc3327e2569
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-12 18:45:49 +0500

    Add Dependencies Exports: Create Index File to Streamline Exports for TaskNode, SubtaskManager, DependencyGraph, and DependencyManager Components

---
+++ src/components/dependencies/index.ts
@@ -0,0 +1,5 @@
+// <== DEPENDENCIES COMPONENTS EXPORTS ==>
+export { default as TaskNode } from "./TaskNode";
+export { default as SubtaskManager } from "./SubtaskManager";
+export { default as DependencyGraph } from "./DependencyGraph";
+export { default as DependencyManager } from "./DependencyManager";
