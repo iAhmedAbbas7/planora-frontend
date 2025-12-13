53fbf7cb28464f82a188cb103d791a9f3825ada6
Ahmed Abbas <iahmedabbas7@gmail.com>
2025-12-13 13:23:49 +0500

    Add Focus Components Index: Create an Index File to Export FocusMode, FocusStats, and PomodoroTimer Components for Streamlined Imports and Improved Organization in the Focus Module

---
+++ src/components/focus/index.ts
@@ -0,0 +1,5 @@
+// <== FOCUS COMPONENTS INDEX ==>
+export { default as FocusMode } from "./FocusMode";
+export { default as FocusStats } from "./FocusStats";
+export { default as PomodoroTimer } from "./PomodoroTimer";
+
